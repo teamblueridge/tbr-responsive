454259fc4719eac1d5ebfb0042be1440f7afcbb5
Henry-01 <hendrik.boone1@gmail.com>
2013-03-31 10:18:35 +0200

    big update
    Change-Id: I1af2aacb9ced327708661fbe50ac2bf7ab41648a

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
-Team BlueRidge Child Theme
-==========================
+[![][logo]]Responsive Theme
 
-http://themeid.com/responsive-theme/
-WordPress child theme based on the Responsive theme. GPLv3 Licensed.
-Copyright restrictions on the logo picture.
+Child theme for WordPress main site
+===================================
+Crafted by Henry_01 for Team BlueRidge. GPLv3 licensed. Copyright restrictions on logo and identity. 
+
+About
+----
+The main style used for the Team Blueridge homepage. This is a child theme and relies on the responsive parent theme from [themeid].
+For more info head over to the [website].
+
+Changelog
+---------
+* Version 2.1: big restyle of the commenting layout. Removing of lots of whitespace and useless text. Deleted post navigation in single posts.
+* Version 2: big update after breakup of parent theme. Huge cleanup and update but no added features.
+* Version 1: initial release
+
+[logo]: http://teamblueridge.com/wp-content/themes/teamblueridge/default-logo.png
+[themeid]: http://themeid.com/responsive-theme/
+[website]: http://teamblueridge.com
--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
 
+//Allow shortcode execution in widgets
 add_filter('widget_text', 'do_shortcode');
 
+//Custom Post Meta with name, date, tag and comment numbers
 if (!function_exists('responsive_post_meta_data')) :
 
 function responsive_post_meta_data() {
@@ -30,6 +32,46 @@ function responsive_post_meta_data() {
 }
 endif;
 
+//Customized comment layout, needed to reorganize/remove some parts
+function tbr_comment($comment, $args, $depth) {
+		$GLOBALS['comment'] = $comment;
+		extract($args, EXTR_SKIP);
+
+		if ( 'div' == $args['style'] ) {$tag = 'div';$add_below = 'comment';} 
+		else {$tag = 'li';$add_below = 'div-comment';}
+?>
+		<<?php echo $tag ?> <?php comment_class(empty( $args['has_children'] ) ? '' : 'parent') ?> id="comment-<?php comment_ID() ?>">
+		<?php if ( 'div' != $args['style'] ) : ?>
+		<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
+		<?php endif; ?>
+		<div class="comment-author vcard">
+		<?php if ($args['avatar_size'] != 0) echo get_avatar( $comment, $args['avatar_size'] ); ?>
+		<?php printf(get_comment_author_link()) ?> <?php edit_comment_link(__('(Edit)'),'  ','' ) ?>
+		<div class="comment-answer">
+		<?php comment_reply_link(array_merge( $args, array('add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+		</div>
+		</div>
+<?php if ($comment->comment_approved == '0') : ?>
+		<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.') ?></em>
+		<br />
+<?php endif; ?>
+
+		<div class="comment-metadata"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
+			<?php
+				/* translators: 1: date, 2: time */
+				printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?></a>
+		</div>
+
+		<?php comment_text() ?>
+	
+		<?php if ( 'div' != $args['style'] ) : ?>
+		</div>
+		<?php endif; ?>
+<?php
+        }
+
+
+//Nothing special, just to allow different logo image size
 if (!function_exists('responsive_setup')):
     function responsive_setup() {
     
@@ -88,6 +130,7 @@ if (!function_exists('responsive_setup')):
     }
 endif;
 
+//Let's load our own favicon
 function childtheme_favicon() { ?><link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri() ?>/images/favicon.ico"><?php }
 add_action('wp_head', 'childtheme_favicon');
 
